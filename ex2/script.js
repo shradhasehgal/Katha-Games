3a6d4a24709cd0dd8c4f75491c1d46cb2f238af5
Shradha <shradha.sgl@gmail.com>
2019-07-03 11:45:35 +0530

    Ex2 completed

--- ex2/script.js
+++ ex2/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 
-    var m0 =  "Yayy! By cleaning the waterbody, you have given Aan fresh drinking water!"
-    var m1 = "Nooooo! The poacher is after Aan's ivory tusks!";
-    var m2 = "Congrats! By planting more trees, you have saved Aan!";
+    // var m0 =  "Yayy! By cleaning the waterbody, you have given Aan fresh drinking water!"
+    // var m1 = "Nooooo! The poacher is after Aan's ivory tusks!";
+    // var m2 = "Congrats! By planting more trees, you have saved Aan!";
     
     $("img").click(function(){
         var id = $(this).attr('id');
         var message; 
         if(id == 1)
         {
-            message = m1;
+            //message = m1;
             $('#'+id).css({
                 'border': '10px solid red',
                 'border-radius' : '100%',
@@ -18,20 +18,22 @@ $(document).ready(function(){
 
         else
         {
-            if(id==0)
-                message = m0;
-            else message = m2;
+            //if(id==0)
+            //   message = m0;
+            //else message = m2;
             $('#'+id).css({
                 'border': '10px solid green',
                 'border-radius' : '100%',
             });
         }
-        document.getElementById("legend").innerHTML = message;
-        $("#info").css("display", "flex");
+        //document.getElementById("legend").innerHTML = message;
+        //$("#info").css("display", "flex");
 
-        
+        var change1 ='an'+id;
         var change = 'ans'+ id;
         $('#'+change).show();
+        $('#'+change1).show();
+        
         del1 = (id + 1)%3;
         del2 = (id + 2)%3;
         $('#'+del1).css({
@@ -41,9 +43,13 @@ $(document).ready(function(){
             'border': 'none',
         });
 
+        var del11 ='an' + del1;
+        var del12 ='an' + del2;
         del1 = 'ans' + del1;
         del2 = 'ans' + del2;
         $('#'+del1).hide();
         $('#'+del2).hide();
+        $('#'+del11).hide();
+        $('#'+del12).hide();
     });
 });
